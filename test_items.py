dda4eb9128ddf80de85f18c52077eb5ebe70f90f
AndreyMuravin <61628479+AndreyMuravin@users.noreply.github.com>
2020-03-01 16:54:48 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -5,5 +5,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_find_basket_button(browser):
     browser.get(link)
-    browser.find_element_by_css_selector("button.btn-add-to-basket")
+    assert browser.find_element_by_css_selector("button.btn-add-to-basket")
 
