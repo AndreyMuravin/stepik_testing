186ec5b0ccb187e07482a283b7ba62aff18effa7
AndreyMuravin <61628479+AndreyMuravin@users.noreply.github.com>
2020-03-01 16:52:33 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,6 +1,6 @@
 
 
-link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
 def test_find_basket_button(browser):
